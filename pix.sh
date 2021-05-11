859f8804b791884ce839a004cbe1fdc24064ab14
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2021-05-11 23:06:39 +0700

    Create pix.sh

---
+++ pix.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/PixelPlusUI-Devices/device_xiaomi_ginkgo device/xiaomi/ginkgo
+git clone --depth=1 https://github.com/Bt-aja/android_kernel_xiaomi_ginkgo kernel/xiaomi/ginkgo
+git clone https://github.com/PixelPlusUI-Devices/vendor_xiaomi_ginkgo vendor/xiaomi/ginkgo
+rm -rf hardware/qcom-caf/sm8150/display
+git clone https://github.com/Bt-aja/hardware_qcom_display.git -b arrow-11.0-caf-sm6125 hardware/qcom-caf/sm8150/display
+git clone https://github.com/Bt-aja/android_vendor_miuicamera  vendor/miuicamera
+git clone https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang.git prebuilts/clang/host/linux-x86/clang-sdllvm
