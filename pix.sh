54eafeb306512f048ed41f1aba20fcbf6dd91df8
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2021-05-24 01:38:11 +0700

    Update pix.sh

--- pix.sh
+++ pix.sh
@@ -4,6 +4,6 @@ git clone https://github.com/PixelPlusUI-Devices/vendor_xiaomi_ginkgo vendor/xia
 rm -rf hardware/qcom-caf/sm8150/display
 git clone https://github.com/Bt-aja/hardware_qcom_display.git -b arrow-11.0-caf-sm6125 hardware/qcom-caf/sm8150/display
 git clone https://github.com/Bt-aja/android_vendor_miuicamera  vendor/miuicamera
-git clone https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang.git prebuilts/clang/host/linux-x86/clang-sdllvm
+git clone https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang.git -b ruby-12 prebuilts/clang/host/linux-x86/clang-sdllvm
 . build/envsetup.sh
 lunch aosp_ginkgo-userdebug
