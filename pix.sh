02c3250e04e1f93165cef646c92f1128c5acdcfe
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2021-08-11 02:16:54 +0700

    Update pix.sh

--- pix.sh
+++ pix.sh
@@ -5,3 +5,4 @@ rm -rf hardware/qcom-caf/sm8150/display
 git clone https://github.com/Bt-aja/hardware_qcom_display.git -b arrow-11.0-caf-sm6125 hardware/qcom-caf/sm8150/display
 git clone https://github.com/Bt-aja/android_vendor_miuicamera  vendor/miuicamera
 git clone https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang.git -b ruby-12 prebuilts/clang/host/linux-x86/clang-sdllvm
+cd device/xiaomi/ginkgo && git revert 7438b807d7ec05b88e3e9a3fdb72e7378ac1e773 && cd ../../..
