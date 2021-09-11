886b78e7ac82991fd555ed504c760d64802801e8
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2021-09-11 18:02:41 +0700

    Create tol.sh

---
+++ tol.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/PixelPlusUI-Devices/device_xiaomi_ginkgo device/xiaomi/ginkgo
+git clone https://github.com/PixelPlusUI-Devices/vendor_xiaomi_ginkgo vendor/xiaomi/ginkgo
+git clone https://github.com/LineageOS/android_tools_extract-utils tools/extract-utils
